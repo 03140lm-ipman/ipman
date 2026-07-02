ebf6f01608d02b558b5e62c90d7b77e99d500b80
03140lm-ipman <03140lm@gmail.com>
2026-07-02 18:26:39 +0200

    Create MainActivity.kt

---
+++ app/app/src/main/java/com/ipman/gps/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ipman.gps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Text("GPS Tracker работает 🚀")
+                }
+            }
+        }
+    }
+}
